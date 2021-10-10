3ecc934267dff1f096ce13310b3114f0c80bd5bb
AbdElRahmanYehya <a.yehya.aboelella@gmail.com>
2021-10-10 22:33:53 +0200

    Shows the deck name with add new question option

---
+++ components/IndividualDeck.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, AsyncStorage } from 'react-native'
+import { purple, white } from '../utils/colors'
+import { showEntries, removeEntry, addQuestion, showEntries2, DECKS_STORAGE_KEY } from '../utils/api'
+
+
+class DeckList extends React.Component{
+//const a = this.props['route'].params.entryId.key
+render() {
+	const id = this.props['route'].params.entryId.key
+	console.log('aa', this.props['route'].params.entryId.key)
+		return (
+			<View>
+				<Text>{this.props['route'].params.entryId.key}</Text>
+				<TouchableOpacity 
+			      	style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.androidSubmitBtn}
+			      	onPress={() => this.props.navigation.navigate('NewQuestion', {entryId: {id}})}>
+						<Text style={styles.submitBtnText}>Add New Question</Text>
+				</TouchableOpacity>
+			</View>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  iosSubmitBtn: {
+		backgroundColor: purple,
+		padding: 10,
+		borderRadius: 7,
+		height :45,
+		marginLeft: 40,
+		marginRight: 40,
+	},
+	androidSubmitBtn: {
+		backgroundColor: purple,
+		padding: 10,
+		borderRadius: 2,
+		height :45,
+		marginLeft: 30,
+		marginRight: 30,
+		alignSelf: 'flex-end',
+		justifyContent: 'center',
+	},
+	submitBtnText: {
+		color: white,
+		fontSize: 22,
+		textAlign: 'center',
+	},
+	decks: {
+		color: 'black',
+		fontSize: 40,
+		textAlign: 'center',
+	},
+	cards: {
+		color: 'grey',
+		fontSize: 30,
+		textAlign: 'center',
+	},
+});
+
+export default  DeckList
